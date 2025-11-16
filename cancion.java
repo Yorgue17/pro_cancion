81f0782c245acdf45b8a737dee18ca8df4fb43fb
yordany <gyordany2001@gmail.com>
2025-11-15 18:28:54 -0600

    se agrego el codigo de cancion, usuario, gestorUsuario

--- cancion.java
+++ cancion.java
@@ -1,4 +1,43 @@
-package Proyecto_cancion;
 public class cancion {
-    
+    private int id;
+    private int userId;  // Clavee
+    private String titulo;
+    private String artista;
+
+    public cancion(int id, int userId, String titulo, String artista) {
+        this.id = id;
+        this.userId = userId;
+        this.titulo = titulo;
+        this.artista = artista;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + id + "] 🎵 " + titulo + " - " + artista;
+    }
 }
+
--- usuario.java
+++ usuario.java
@@ -1,4 +1,28 @@
-package Proyecto_cancion;
+public static void main(String[] args) {
+    // Aquí puedes agregar el código que deseas ejecutar en el método main
+}
+
 public class usuario {
+    private int id;
+    private String nombre;
+    private String password;
     
+
+    public usuario(int id, String nombre, String password) {
+        this.id = id;
+        this.nombre = nombre;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
